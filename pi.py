0463d22bf0146cbabbcf9a35f59aacb105fe7643
SugiyamaKouta76 <pikkalarge@gmail.com>
2025-10-14 12:03:17 +0900

    第2問目

--- pi.py
+++ pi.py
@@ -1,7 +1,33 @@
-text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
-"""
+text = [
+    "How", "I", "want", "a", "drink", "alcoholic" ,"of" ,"course","after","the","heavy","chapters","involving","quantum","mechanics","All","of","thy","geometry","Herr","Planck","is","fairly","hard"
+]
 
+
+a = list(map(len, [text[0]]))
+b = list(map(len, [text[1]]))
+c = list(map(len, [text[2]]))
+d = list(map(len, [text[3]]))
+e = list(map(len, [text[4]]))
+f = list(map(len, [text[5]]))
+g = list(map(len, [text[6]]))
+h = list(map(len, [text[7]]))
+i = list(map(len, [text[8]]))
+j = list(map(len, [text[9]]))
+k = list(map(len, [text[10]]))
+l = list(map(len, [text[11]]))
+m = list(map(len, [text[12]]))
+n = list(map(len, [text[13]]))
+o = list(map(len, [text[14]]))
+p = list(map(len, [text[15]]))
+q = list(map(len, [text[16]]))
+r = list(map(len, [text[17]]))
+s = list(map(len, [text[18]]))
+t = list(map(len, [text[19]]))
+u = list(map(len, [text[20]]))
+v = list(map(len, [text[21]]))
+w = list(map(len, [text[22]]))
+x = list(map(len, [text[23]]))
+
+print(*(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x))
 # TODO
 
