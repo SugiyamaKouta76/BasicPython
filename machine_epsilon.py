301f801ccf750ed32b5d2dcae7d64b444eaf545f
SugiyamaKouta76 <pikkalarge@gmail.com>
2025-10-28 18:10:19 +0900

    未完成

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,11 @@
-# TODO
+# rangeの値を（分母を）大きくしていきe==1になったら処理を終了させる、どの段階で１になるかを確かめためにmachine という変数に＋＝１で１ループするごとに１足していく
+
+machine = 0
+e = 0
+for i in range(100):
+  if e != 0:
+        e = 1 / i
+        machine += 1
+print(machine)
+
+
