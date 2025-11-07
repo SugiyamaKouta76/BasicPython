3e533cc64276eabba6b23f23d9de380ac2445a3f
SugiyamaKouta76 <pikkalarge@gmail.com>
2025-11-07 21:12:05 +0900

    出力はされるけどおそらく違う

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,11 +1,16 @@
-# rangeの値を（分母を）大きくしていきe==1になったら処理を終了させる、どの段階で１になるかを確かめためにmachine という変数に＋＝１で１ループするごとに１足していく
+# rangeの値を（分母を）大きくしていきe==1になったら処理を終了させる、どの段階で１になるかを確かめためにmachine という変数に＋＝１で１ループする
+e = 1
+a = 0
+number = 1 + e
 
-machine = 0
-e = 0
 for i in range(100):
-  if e != 0:
-        e = 1 / i
-        machine += 1
-print(machine)
+    if number != 1:
+        e = 1 / (2 ** i)
+        number = 1 + e
+        a += 1
+
+b = 1 / (2 ** a)
+print(e)
+
 
 
